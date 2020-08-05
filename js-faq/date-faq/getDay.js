ba908a1a9927abdaa5c6ef15a042e970ee078774
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-05 21:23:32 +0600

    getday js date

---
+++ js-faq/date-faq/getDay.js
@@ -0,0 +1,10 @@
+var A = new Date('October 03, 1996 05:35:32'); 
+  
+var B = A.getDay(); 
+
+console.log(B); 
+
+//The date.getDay() method is used to fetch the day of a week(0 to 6) from a given Date object
+
+// Result
+// 4
