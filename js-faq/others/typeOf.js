148589130e9b48e948afd8f10edb5f7237875a7d
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-13 22:47:49 +0600

    typeof in js

---
+++ js-faq/others/typeOf.js
@@ -0,0 +1,9 @@
+var number = 123;
+var string = 'My String';
+var bool = true;
+var object = {name:'My Obj', Id:120};
+
+console.log(typeof number);
+console.log(typeof string);
+console.log(typeof bool);
+console.log(typeof object);
